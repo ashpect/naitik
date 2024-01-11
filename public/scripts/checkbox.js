38263ca63bb027af6c5fa6b05270dcd43ce28103
ashpect <ashishndiitr@gmail.com>
2024-01-11 17:18:40 +0530

    hi

--- public/scripts/checkbox.js
+++ public/scripts/checkbox.js
@@ -1,11 +1,12 @@
-let count = 0;
-let allInputs = document.getElementsByTagName("input");
-if(allInputs.length > 0){
-    for(let i=0; i<allInputs.length; i++){
-        if (allInputs[i].type = 'checkbox'){
-          allInputs[i].checked = true;
-          count++;
-        }
-        }
-    }
-alert("Unchecked " + allInputs.length + "boxes , which were a potential to be misled to be used.");
+// let count = 0;
+// let allInputs = document.getElementsByTagName("input");
+// if(allInputs.length > 0){
+//     for(let i=0; i<allInputs.length; i++){
+//         if (allInputs[i].type = 'checkbox'){
+//           allInputs[i].checked = true;
+//           count++;
+//         }
+//         }
+//     }
+// alert("Unchecked " + allInputs.length + "boxes , which were a potential to be misled to be used.");
+alert("Hi, I am under the water. Please save me.")
