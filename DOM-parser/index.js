3f1a500ec3c790aa59b9b22e0e7ceae0a70e18a9
Ashutosh <cyberboy9971@gmail.com>
2024-01-19 03:19:38 +0530

    Add contents array

--- DOM-parser/index.js
+++ DOM-parser/index.js
@@ -1,12 +1,15 @@
 // var node_types = ["IGNORE_ELEMENT", "BLOCK_ELEMENT", "INLINE_ELEMENT", "TEXT_NODE", "COMMENT_NODE"];
 
-// Which elements should be ignored??? All the elements are Copilot's suggestions
+//FIXME: Which elements should be ignored??? All the elements are Copilot's suggestions
 var IGNORE_ELEMENTS = ["SCRIPT", "STYLE", "LINK", "NOSCRIPT", "IFRAME", "OBJECT", "EMBED", "PARAM", "SOURCE", "CANVAS", "SVG", "USE", "TRACK", "INPUT", "TEXTAREA", "SELECT", "OPTION", "OPTGROUP", "BUTTON", "LABEL", "FIELDSET", "LEGEND", "BR", "HR"];
-// META HEAD YEH sab daalna hai??
+//FIXME: META HEAD YEH sab daalna hai??
 
 var BLOCK_ELEMENTS = ["DIV", "P", "H1", "H2", "H3", "H4", "H5", "H6", "UL", "OL", "LI", "DL", "DT", "DD", "TABLE", "THEAD", "TBODY", "TFOOT", "TR", "TH", "TD", "FORM", "HEADER", "FOOTER", "NAV", "SECTION", "ARTICLE", "ASIDE", "DETAILS", "DIALOG", "SUMMARY", "FIGURE", "FIGCAPTION", "ADDRESS", "MAIN", "HR", "PRE", "BLOCKQUOTE"];
 
 var INLINE_ELEMENTS = ["SPAN", "A", "STRONG", "B", "I", "EM", "MARK", "SMALL", "DEL", "INS", "SUB", "SUP", "Q", "CITE", "DFN", "ABBR", "TIME", "CODE", "VAR", "SAMP", "KBD", "STRONG", "B", "I", "EM", "MARK", "SMALL", "DEL", "INS", "SUB", "SUP", "Q", "CITE", "DFN", "ABBR", "TIME", "CODE", "VAR", "SAMP", "KBD", "S", "U", "WBR", "IMG", "BR", "INPUT", "TEXTAREA", "SELECT", "OPTION", "OPTGROUP", "BUTTON", "LABEL", "FIELDSET", "LEGEND", "IFRAME", "OBJECT", "EMBED", "PARAM", "SOURCE", "CANVAS", "SVG", "USE", "TRACK", "INPUT", "TEXTAREA", "SELECT", "OPTION", "OPTGROUP", "BUTTON", "LABEL", "FIELDSET", "LEGEND"];
+
+// Isko set banado to avoid redundant data
+var contents = [];
 
 function cleanText(text) {
     const continuousSpaceOverTwoCharactorRule = /\s{2,}/g;
@@ -28,9 +31,12 @@ function createDOMTreeAndUpdateAttributes(rootElement, parentId, node) {
             tag: childElement.tagName,
             children: [],
             content: !IGNORE_ELEMENTS.includes(childElement.tagName) ? cleanText(childElement.textContent) : "",
+            //FIXME: Yeh kuchh gadbad hai to be fixed
             nodeType: IGNORE_ELEMENTS.includes(childElement.tagName) ? "IGNORE_ELEMENT" : BLOCK_ELEMENTS.includes(childElement.tagName) ? "BLOCK_ELEMENT" : INLINE_ELEMENTS.includes(childElement.tagName) ? "INLINE_ELEMENT" : "TEXT_NODE"
             });
-  
+        if (node.nodeType == "TEXT_NODE" || node.nodeType == "INLINE_ELEMENT" || node.nodeType == "BLOCK_ELEMENT"){
+            contents.push(node.content);
+        }
         // Recursively traverse and update child elements
         traverseAndUpdate(childElement, childNid, node.children[i]);
       }
@@ -59,6 +65,7 @@ var domTree = createDOMTreeAndUpdateAttributes(rootElement, node.nid, node);
 
 // Output the DOM tree
 console.log(JSON.stringify(domTree, null, 2));
+console.log(contents);
   
 
 function getElementByNaitikID(nid) {
