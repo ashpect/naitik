80b10a5f108102d789e4617d0d2bcba0ec448450
Ashutosh <cyberboy9971@gmail.com>
2024-01-19 03:08:25 +0530

    Clean content with regex and add types of nodes

--- DOM-parser/index.js
+++ DOM-parser/index.js
@@ -1,3 +1,20 @@
+// var node_types = ["IGNORE_ELEMENT", "BLOCK_ELEMENT", "INLINE_ELEMENT", "TEXT_NODE", "COMMENT_NODE"];
+
+// Which elements should be ignored??? All the elements are Copilot's suggestions
+var IGNORE_ELEMENTS = ["SCRIPT", "STYLE", "LINK", "NOSCRIPT", "IFRAME", "OBJECT", "EMBED", "PARAM", "SOURCE", "CANVAS", "SVG", "USE", "TRACK", "INPUT", "TEXTAREA", "SELECT", "OPTION", "OPTGROUP", "BUTTON", "LABEL", "FIELDSET", "LEGEND", "BR", "HR"];
+// META HEAD YEH sab daalna hai??
+
+var BLOCK_ELEMENTS = ["DIV", "P", "H1", "H2", "H3", "H4", "H5", "H6", "UL", "OL", "LI", "DL", "DT", "DD", "TABLE", "THEAD", "TBODY", "TFOOT", "TR", "TH", "TD", "FORM", "HEADER", "FOOTER", "NAV", "SECTION", "ARTICLE", "ASIDE", "DETAILS", "DIALOG", "SUMMARY", "FIGURE", "FIGCAPTION", "ADDRESS", "MAIN", "HR", "PRE", "BLOCKQUOTE"];
+
+var INLINE_ELEMENTS = ["SPAN", "A", "STRONG", "B", "I", "EM", "MARK", "SMALL", "DEL", "INS", "SUB", "SUP", "Q", "CITE", "DFN", "ABBR", "TIME", "CODE", "VAR", "SAMP", "KBD", "STRONG", "B", "I", "EM", "MARK", "SMALL", "DEL", "INS", "SUB", "SUP", "Q", "CITE", "DFN", "ABBR", "TIME", "CODE", "VAR", "SAMP", "KBD", "S", "U", "WBR", "IMG", "BR", "INPUT", "TEXTAREA", "SELECT", "OPTION", "OPTGROUP", "BUTTON", "LABEL", "FIELDSET", "LEGEND", "IFRAME", "OBJECT", "EMBED", "PARAM", "SOURCE", "CANVAS", "SVG", "USE", "TRACK", "INPUT", "TEXTAREA", "SELECT", "OPTION", "OPTGROUP", "BUTTON", "LABEL", "FIELDSET", "LEGEND"];
+
+function cleanText(text) {
+    const continuousSpaceOverTwoCharactorRule = /\s{2,}/g;
+    // const textsReplaced = text.replace(continuousSpaceOverTwoCharactorRule, ' ')).replace('\n', ''));
+    const newText = text.replace(continuousSpaceOverTwoCharactorRule, ' ').replace('\n', '');
+    return newText;
+}
+
 function createDOMTreeAndUpdateAttributes(rootElement, parentId, node) {
     function traverseAndUpdate(element, parent, node) {
       for (var i = 0; i < element.children.length; i++) {
@@ -10,7 +27,8 @@ function createDOMTreeAndUpdateAttributes(rootElement, parentId, node) {
             nid: childElement.getAttribute('naitik-id'),
             tag: childElement.tagName,
             children: [],
-            content: childElement.textContent
+            content: !IGNORE_ELEMENTS.includes(childElement.tagName) ? cleanText(childElement.textContent) : "",
+            nodeType: IGNORE_ELEMENTS.includes(childElement.tagName) ? "IGNORE_ELEMENT" : BLOCK_ELEMENTS.includes(childElement.tagName) ? "BLOCK_ELEMENT" : INLINE_ELEMENTS.includes(childElement.tagName) ? "INLINE_ELEMENT" : "TEXT_NODE"
             });
   
         // Recursively traverse and update child elements
@@ -21,32 +39,26 @@ function createDOMTreeAndUpdateAttributes(rootElement, parentId, node) {
     // Update the root element
     rootElement.setAttribute('naitik-id', parentId);
   
-    // Create the DOM tree
-    // var tree = {
-    //   nid: parentId,
-    //   tag: rootElement.tagName,
-    //   children: []
-    // };
-  
     traverseAndUpdate(rootElement, parentId, node);
     return node;
   }
-  
-  // Get the root element of the document
-  var rootElement = document.documentElement;
 
-  var node = {
-    nid: 'root',
-    tag: rootElement.tagName,
-    children: [],
-    content: rootElement.textContent
-  }
-  
-  // Create the DOM tree and update attributes
-  var domTree = createDOMTreeAndUpdateAttributes(rootElement, node.nid, node);
-  
-  // Output the DOM tree
-  console.log(JSON.stringify(domTree, null, 2));
+// Get the root element of the document
+var rootElement = document.documentElement;
+
+var node = {
+nid: 'root',
+tag: rootElement.tagName,
+children: [],
+content: rootElement.textContent,
+nodeType: "HTML_ELEMENT"
+}
+
+// Create the DOM tree and update attributes
+var domTree = createDOMTreeAndUpdateAttributes(rootElement, node.nid, node);
+
+// Output the DOM tree
+console.log(JSON.stringify(domTree, null, 2));
   
 
 function getElementByNaitikID(nid) {
