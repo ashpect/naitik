ef5bbac6f9b44d17d40f7797bf5eb40337c0d33e
Ashutosh <cyberboy9971@gmail.com>
2024-01-21 20:26:53 +0530

    Cleaned and added id to contents

--- z_misc/DOM-parser/index.js
+++ z_misc/DOM-parser/index.js
@@ -1,12 +1,12 @@
 // var node_types = ["IGNORE_ELEMENT", "BLOCK_ELEMENT", "INLINE_ELEMENT", "TEXT_NODE", "COMMENT_NODE"];
 
 //FIXME: Which elements should be ignored??? All the elements are Copilot's suggestions
-var IGNORE_ELEMENTS = ["SCRIPT", "STYLE", "LINK", "NOSCRIPT", "IFRAME", "OBJECT", "EMBED", "PARAM", "SOURCE", "CANVAS", "SVG", "USE", "TRACK", "INPUT", "TEXTAREA", "SELECT", "OPTION", "OPTGROUP", "BUTTON", "LABEL", "FIELDSET", "LEGEND", "BR", "HR"];
+var IGNORE_ELEMENTS = ["SCRIPT", "STYLE", "LINK", "NOSCRIPT", "IFRAME", "OBJECT", "EMBED", "PARAM", "SOURCE", "CANVAS", "SVG", "USE", "TRACK", "INPUT", "TEXTAREA", "SELECT", "OPTION", "OPTGROUP", "BUTTON", "LABEL", "FIELDSET", "LEGEND", "BR", "HR", "META", "HEAD"];
 //FIXME: META HEAD YEH sab daalna hai??
 
 var BLOCK_ELEMENTS = ["DIV", "P", "H1", "H2", "H3", "H4", "H5", "H6", "UL", "OL", "LI", "DL", "DT", "DD", "TABLE", "THEAD", "TBODY", "TFOOT", "TR", "TH", "TD", "FORM", "HEADER", "FOOTER", "NAV", "SECTION", "ARTICLE", "ASIDE", "DETAILS", "DIALOG", "SUMMARY", "FIGURE", "FIGCAPTION", "ADDRESS", "MAIN", "HR", "PRE", "BLOCKQUOTE"];
 
-var INLINE_ELEMENTS = ["SPAN", "A", "STRONG", "B", "I", "EM", "MARK", "SMALL", "DEL", "INS", "SUB", "SUP", "Q", "CITE", "DFN", "ABBR", "TIME", "CODE", "VAR", "SAMP", "KBD", "STRONG", "B", "I", "EM", "MARK", "SMALL", "DEL", "INS", "SUB", "SUP", "Q", "CITE", "DFN", "ABBR", "TIME", "CODE", "VAR", "SAMP", "KBD", "S", "U", "WBR", "IMG", "BR", "INPUT", "TEXTAREA", "SELECT", "OPTION", "OPTGROUP", "BUTTON", "LABEL", "FIELDSET", "LEGEND", "IFRAME", "OBJECT", "EMBED", "PARAM", "SOURCE", "CANVAS", "SVG", "USE", "TRACK", "INPUT", "TEXTAREA", "SELECT", "OPTION", "OPTGROUP", "BUTTON", "LABEL", "FIELDSET", "LEGEND"];
+var INLINE_ELEMENTS = ["SPAN", "A", "STRONG", "B", "I", "EM", "MARK", "PARAM", "SOURCE", "CANVAS", "TRACK", "INPUT", "TEXTAREA", "SELECT", "OPTION", "BUTTON", "LABEL", "FIELDSET", "LEGEND"];
 
 // Isko set banado to avoid redundant data
 var contents = [];
@@ -35,7 +35,11 @@ function createDOMTreeAndUpdateAttributes(rootElement, parentId, node) {
             nodeType: IGNORE_ELEMENTS.includes(childElement.tagName) ? "IGNORE_ELEMENT" : BLOCK_ELEMENTS.includes(childElement.tagName) ? "BLOCK_ELEMENT" : INLINE_ELEMENTS.includes(childElement.tagName) ? "INLINE_ELEMENT" : "TEXT_NODE"
             });
         if (node.nodeType == "TEXT_NODE" || node.nodeType == "INLINE_ELEMENT" || node.nodeType == "BLOCK_ELEMENT"){
-            contents.push(node.content);
+            var content = {
+                nid: childElement.getAttribute('naitik-id'),
+                text: cleanText(childElement.textContent)
+            }
+            contents.push(content);
         }
         // Recursively traverse and update child elements
         traverseAndUpdate(childElement, childNid, node.children[i]);
@@ -53,11 +57,11 @@ function createDOMTreeAndUpdateAttributes(rootElement, parentId, node) {
 var rootElement = document.documentElement;
 
 var node = {
-nid: 'root',
-tag: rootElement.tagName,
-children: [],
-content: rootElement.textContent,
-nodeType: "HTML_ELEMENT"
+  nid: 'root',
+  tag: rootElement.tagName,
+  children: [],
+  content: rootElement.textContent,
+  nodeType: "HTML_ELEMENT"
 }
 
 // Create the DOM tree and update attributes
