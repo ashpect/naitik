7f0639c66d00e07656f1ca9b33609dd7278587db
percy <vanshuppal2002@gmail.com>
2024-02-27 23:00:36 +0530

    OCR

--- naitik_mini/src/components/pict.tsx
+++ naitik_mini/src/components/pict.tsx
@@ -3,12 +3,17 @@ import axios from "axios";
 
 function OCR() {
   const [image, setImage] = useState<string | null>(null); // Allow null values
+  const [ocrResult, setOcrResult] = useState<string | null>(null);
 
   // useEffect to retrieve image from local storage on component mount
   useEffect(() => {
     const uploadedImage = localStorage.getItem("uploadedImage");
+    const ocrResult = localStorage.getItem("ocrResult");
     if (uploadedImage) {
       setImage(uploadedImage);
+    }
+    if (ocrResult) {
+      setOcrResult(ocrResult);
     }
   }, []); // Empty dependency array to run only on mount
 
@@ -16,11 +21,12 @@ function OCR() {
     if (image) {
       try {
         // Send image using Axios
-        await axios.post("http://localhost:5000/ocr", { image });
+        const response = await axios.post("http://localhost:5000/ocr", { image });
         console.log("Image sent to localhost:5000/ocr");
-  
-        // Reset image in local storage and state
         localStorage.removeItem("uploadedImage");
+        const ocrResult = response.data; // Assuming response.data contains OCR result
+        localStorage.setItem("ocrResult", ocrResult);
+        setOcrResult(ocrResult);
         setImage(null);
       } catch (error) {
         console.error("Error sending image:", error);
@@ -30,7 +36,9 @@ function OCR() {
 
   const goBack = () => {
     localStorage.removeItem("uploadedImage");
+    localStorage.removeItem("ocrResult");
     setImage(null);
+    setOcrResult(null);
   }
   
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,9 +47,7 @@ function OCR() {
       const reader = new FileReader();
       reader.onload = (event) => {
         const base64String = event.target?.result as string;
-        console.log(base64String)
         setImage(base64String);
-        
         localStorage.setItem("uploadedImage", base64String);
       };
       reader.readAsDataURL(file);
@@ -60,12 +66,17 @@ function OCR() {
           </div>
         </div>
         <div className="buttons">
-          {image ? (
+          {ocrResult ? (
             <>
-            <button className="secondary-button" onClick={goBack}> Go Back</button>
-            <button className="primary-button" onClick={handleUpload}>
-              OCR
-            </button>
+              <button className="secondary-button" onClick={goBack}> Go Back</button>
+              <div>{ocrResult}</div>
+            </>
+          ) : image ? (
+            <>
+              <button className="secondary-button" onClick={goBack}> Go Back</button>
+              <button className="primary-button" onClick={handleUpload}>
+                OCR
+              </button>
             </>
           ) : (
             <label htmlFor="upload-button" className="primary-button">
